d3081e40551ffaacf3b66474a00cb4efbec17ce9
Clément QUIQUANDON <63378551+Clemmnt69@users.noreply.github.com>
2024-11-14 15:39:25 +0000

    Test mock install.

--- ProductApp.Tests/obj/Debug/net8.0/ProductApp.Tests.AssemblyInfo.cs
+++ ProductApp.Tests/obj/Debug/net8.0/ProductApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+163eb5afee22c8e79807c771b2dae8c344d734ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8b18e32eef942a1dec7c79f465decb37720fe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
