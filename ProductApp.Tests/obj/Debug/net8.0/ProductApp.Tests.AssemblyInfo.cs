163eb5afee22c8e79807c771b2dae8c344d734ac
Clément QUIQUANDON <63378551+Clemmnt69@users.noreply.github.com>
2024-11-14 15:26:55 +0000

    Init Files.

--- ProductApp.Tests/obj/Debug/net8.0/ProductApp.Tests.AssemblyInfo.cs
+++ ProductApp.Tests/obj/Debug/net8.0/ProductApp.Tests.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProductApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ProductApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2e94ff699731636732ed5318ab222b48641083")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProductApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProductApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("ProductApp.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ProductApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
